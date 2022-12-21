cdbf794eac95195ef837d666ce71d24afdacbde6
Sumaiya Akter Mim <116157344+mim927628@users.noreply.github.com>
2022-12-21 16:39:46 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+// const num=[23,45,67,89];
+// let txt="";
+// num.forEach(numfunction);
+// document.getElementById('demo').innerHTML=txt;
+
+// function numfunction(value,index,arrey){
+//     txt+=value+"<br>";
+// }
+
